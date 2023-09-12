44597653a60894dcc2e387eae83bf6f50988e8a4
aldoaquino451 <aldoaquino2909@gmail.com>
2023-09-12 16:11:24 +0200

    [add]  controllo if e for con log

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-/*
-1. Dai la possibilità all'utente di inserire la propria email
-2. Crea una lista con tutte le mail da confrontare 
-3. Confronta l'email utente con la lista di email 
-4. Scrivi un testo che conferma l'esito
-*/
 
 /*
 1. Genera un numero random per l'utente 
@@ -14,3 +8,39 @@
 6. Nel caso dell'uguaglianza stampa un messaggio in cui chiedi di ripetere l'operazione 
 7. Ripeti l'operazione
 */
+
+/*
+1. Dai la possibilità all'utente di inserire la propria email
+2. Crea una lista con tutte le mail da confrontare 
+3. Confronta l'email utente con tutti gli elementi della lista ( ciclo for )
+4. Scrivi un testo che conferma l'esito
+*/
+
+
+// 1. 
+const userEmail = prompt('Scrivi la tua e-mail:');
+
+// 2.
+const emailList = [
+  'AldoAquino@gmail.com',
+  'MassimoAzzini@gmail.com',
+  'VahidBan@gmail.com',
+  'GiacomoBechini@gmail.com',
+  'ThomasBortolotti@gmail.com',
+  'CarmeloCammalleri@gmail.com',
+  'AlessandroCingolani@gmail.com',
+];
+
+// 3.
+for ( let i = 0; i < emailList.length; i++ ) {
+
+  console.log(emailList[i]);
+  
+}
+
+
+if ( userEmail === emailList[0] ) {
+ console.log('ok');
+} else {
+  console.log('error');
+}
