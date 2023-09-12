f03b534afeb759ebafd6efd53a3b3e714c1dea26
aldoaquino451 <aldoaquino2909@gmail.com>
2023-09-12 15:53:51 +0200

    [init] index, style, script files

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+1. Dai la possibilità all'utente di inserire la propria email
+2. Crea una lista con tutte le mail da confrontare 
+3. Confronta l'email utente con la lista di email 
+4. Scrivi un testo che conferma l'esito
+*/
+
+/*
+1. Genera un numero random per l'utente 
+2. Genera un numero random per il computer
+3. Confronta i due valori, possiamo ottenere:  a < b  |  a = b  |  a > b
+4. Nel primo caso vince il computer quindi stampa il messaggio di vittoria
+5. Nel terzo caso vince l'utente quindi stampa il messaggio di vittoria
+6. Nel caso dell'uguaglianza stampa un messaggio in cui chiedi di ripetere l'operazione 
+7. Ripeti l'operazione
+*/
